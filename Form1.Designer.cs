0b8b440355dedc2bcb167a2842ddd51a41335802
Fábián Gábor <info@fabiangabor.com>
2021-11-02 22:29:34 +0100

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Pixels
+﻿using System;
+
+namespace Pixels
 {
     partial class Form1
     {
@@ -29,11 +31,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.drawBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.drawPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Items.AddRange(new object[] {"Fill", "Checkerbox(5x5)", "Checkerbox(50x50)", "Noise"});
+            this.listBox.Location = new System.Drawing.Point(12, 12);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(200, 56);
+            this.listBox.TabIndex = 0;
+            // 
+            // drawBtn
+            // 
+            this.drawBtn.Location = new System.Drawing.Point(12, 74);
+            this.drawBtn.Name = "drawBtn";
+            this.drawBtn.Size = new System.Drawing.Size(200, 24);
+            this.drawBtn.TabIndex = 1;
+            this.drawBtn.Text = "draw";
+            this.drawBtn.UseVisualStyleBackColor = true;
+            this.drawBtn.Click += new System.EventHandler(this.drawBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(12, 414);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(200, 24);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // drawPanel
+            // 
+            this.drawPanel.Location = new System.Drawing.Point(218, 12);
+            this.drawPanel.Name = "drawPanel";
+            this.drawPanel.Size = new System.Drawing.Size(570, 426);
+            this.drawPanel.TabIndex = 3;
+            this.drawPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.drawPanel_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.drawPanel);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.drawBtn);
+            this.Controls.Add(this.listBox);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button drawBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Panel drawPanel;
+
+        private System.Windows.Forms.ListBox listBox;
 
         #endregion
     }
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,117 @@ namespace Pixels
         {
             InitializeComponent();
         }
+
+        List<Rectangle> rectanglesRed = new List<Rectangle>();
+        List<Rectangle> rectanglesWhite = new List<Rectangle>();
+
+        private void Form1_Paint(object sender, PaintEventArgs pe)
+        {
+        }
+
+        private void drawBtn_Click(object sender, EventArgs e)
+        {
+            switch (listBox.SelectedIndex)
+            {
+                case 0:
+                    fillArea();
+                    break;
+                case 1:
+                    checkerbox(5);
+                    break;
+                case 2:
+                    checkerbox(50);
+                    break;
+                case 3:
+                    noise(2);
+                    break;
+            }
+        }
+
+        private void fillArea()
+        {
+            drawPanel.BackColor = Color.Red;
+            this.Invalidate(); // force Redraw the form
+        }
+
+        private void checkerbox(int size)
+        {
+            rectanglesRed.Clear();
+            rectanglesWhite.Clear();
+            
+            var countX = drawPanel.Width / size;
+            var countY = drawPanel.Height / size;
+
+            for (int i = 0; i <= countX; i++)
+            {
+                for (int j = 0; j <= countY; j+=2)
+                {
+                    var rectangle = new Rectangle(new Point(i * size, j * size + size * (i%2)), new Size(size, size));
+                    this.rectanglesRed.Add(rectangle);
+                    
+                    rectangle = new Rectangle(new Point(i * size, j * size + size * ((i+1)%2)), new Size(size, size));
+                    this.rectanglesWhite.Add(rectangle);
+                }
+            }
+
+            this.Invalidate(); // force Redraw the form
+        }
+        
+        private void noise(int size)
+        {
+            rectanglesRed.Clear();
+            rectanglesWhite.Clear();
+            
+            var countX = drawPanel.Width / size;
+            var countY = drawPanel.Height / size;
+
+            Random random = new Random();
+            for (int i = 0; i <= countX; i++)
+            {
+                for (int j = 0; j <= countY; j++)
+                {
+                    var rectangle = new Rectangle(new Point(i * size, j * size), new Size(size, size));
+                    
+                    if (random.NextDouble() > 0.5)
+                        this.rectanglesRed.Add(rectangle);
+                    else
+                    {
+                        this.rectanglesWhite.Add(rectangle);
+                    }
+                }
+            }
+
+            this.Invalidate(); // force Redraw the form
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+            this.Paint += drawPanel_Paint;
+        }
+
+        private void drawPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = drawPanel.CreateGraphics();
+            
+            SolidBrush sb = new SolidBrush(Color.Red);
+
+            drawPanel.BackColor = Color.White;
+            foreach (var rectangle in this.rectanglesRed)
+            {
+                g.FillRectangle(sb, rectangle);
+            }
+            
+            sb = new SolidBrush(Color.White);
+            foreach (var rectangle in this.rectanglesWhite)
+            {
+                g.FillRectangle(sb, rectangle);
+            }
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
